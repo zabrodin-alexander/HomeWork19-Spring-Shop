8a6fee2c4f04c9f3b85fcde7b993a45415d44618
Zabro <zabrodin.alexander51@gmail.com>
2025-01-08 11:15:39 +0300

    begin

---
+++ src/main/java/com/example/internetShop/InternetShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.internetShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/internetShop/InternetShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.internetShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
